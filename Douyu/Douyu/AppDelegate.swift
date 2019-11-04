cdabcd3566768dc4c074a071d81301f1b8acecef
Max <860280696@qq.com>
2019-11-04 20:43:17 +0800

    tag

--- Douyu/Douyu/AppDelegate.swift
+++ Douyu/Douyu/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var str = String()
+        str = "哈哈"
+        
+        print("\(str)....\(100)")
+        
+        
+        
         return true
     }
 
