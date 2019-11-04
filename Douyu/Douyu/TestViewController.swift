d5a1ebc1708d71d03d5fec719dbd2441c378b468
Max <860280696@qq.com>
2019-11-04 20:53:52 +0800

    tubiao

---
+++ Douyu/Douyu/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  Douyu
+//
+//  Created by Max on 2019/11/4.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
